714ffd1966b5c0abcd33448f6f4fc97700f17524
ssgad <ssgadde6@gmail.com>
2020-11-24 12:32:43 +1100

    Mobile friendly and Maps API added

--- effects.js
+++ effects.js
@@ -19,3 +19,16 @@ function bMenu() {
       x.className = "nav-bar";
     }
   }
+
+var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 6
+        })
+      });
