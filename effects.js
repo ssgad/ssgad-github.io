3ec3b9a667fd4e2bea286c6d3edcd2556dda1ae4
ssgad <ssgadde6@gmail.com>
2020-12-02 10:54:15 +1100

    Responsiveness

--- effects.js
+++ effects.js
@@ -15,6 +15,7 @@ function bMenu() {
     var x = document.getElementById("navba");
     if (x.className === "nav-bar") {
       x.className += "responsive";
+      
     } else {
       x.className = "nav-bar";
     }
